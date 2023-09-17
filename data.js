41a8ed9761cfb9875d35057494d5ed8624ee7b87
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-09-17 12:36:53 -0500

    added functionality to menu items/cart
    - can now click on items to render them to order summary
    - can remove items from order

--- data.js
+++ data.js
@@ -4,20 +4,26 @@ export const menuArray = [
         ingredients: ["pepperoni", " mushroom", " mozarella"],
         id: 0,
         price: 14,
-        emoji: "🍕"
+        emoji: "🍕",
+        totalprice: 0,
+        quantity: 0
     },
     {
         name: "Hamburger",
         ingredients: ["beef", " cheese", " lettuce"],
         price: 12,
         emoji: "🍔",
-        id: 1
+        id: 1,
+        totalprice: 0,
+        quantity: 0
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 12,
         emoji: "🍺",
-        id: 2
+        id: 2,
+        totalprice: 0,
+        quantity: 0
     }
 ]
--- index.js
+++ index.js
@@ -1,5 +1,76 @@
 import { menuArray } from "./data.js";
 const menu = document.getElementById("menu-container")
+const addBtn = document.querySelectorAll(".add-btn")
+const orderSummary = document.getElementById("order-summary")
+const orderedItemsContainer = document.getElementById("ordered-items")
+const totalOrderPrice = document.getElementById("total-price")
+let orderArray = []
+
+addBtn.forEach(btn => {
+    btn.addEventListener("click", function() {
+        alert("works")
+    })
+})
+
+document.addEventListener("click", (e) => {
+    if(e.target.dataset.add) {
+        addItem(e.target.dataset.add)
+    } else if(e.target.dataset.remove) {
+        removeItem(e.target.dataset.remove)
+    } 
+})
+
+function updateOrder() {
+    renderOrder()
+    console.log(orderArray)
+}
+
+function addItem(itemId) {
+    const itemObj = menuArray.filter(item => {
+        return item.id.toString() === itemId
+    })[0]
+    itemObj.quantity++
+    itemObj.totalprice = itemObj.price * itemObj.quantity
+    if(!orderArray.includes(itemObj)) {
+        orderArray.push(itemObj)
+    }
+    updateOrder()
+}
+
+function removeItem(itemId) {
+    const itemObj = menuArray.filter(item => {
+        return item.id.toString() === itemId
+    })[0]
+    if(itemObj.quantity > 0) {
+        itemObj.quantity--
+    itemObj.totalprice = itemObj.price * itemObj.quantity
+    }
+    orderArray = orderArray.filter(item => {
+        return item.quantity > 0
+    })
+    updateOrder()
+}
+
+function renderOrder() {
+    if(orderArray) {
+        orderSummary.style.visibility = "visible"
+        let orderItems = ""
+        let orderTotal = 0
+        orderArray.forEach(item => {
+            orderItems += `
+            <div class="order-item">
+                <h3 class="order-item-title">${item.name}</h3>
+                <p class="quantity">Quantity: ${item.quantity}</p>
+                <button id="remove-btn" class="remove-btn" data-remove="${item.id}">remove</button>
+                <p class="order-item-price">$ ${item.totalprice}</p>
+            </div>
+            `
+            orderTotal += item.totalprice
+        })
+        orderedItemsContainer.innerHTML = orderItems
+        totalOrderPrice.textContent = `$ ${orderTotal}`
+    }
+}
 
 function renderMenu() {
     return menuArray.forEach(item => {
@@ -11,7 +82,7 @@ function renderMenu() {
                     <p class="menu-item-ingredients">${item.ingredients}</p>
                     <p class="price">$${item.price}</p>
                 </div>
-                <button id="add-btn" class="add-btn">+</button>
+                <button class="add-btn" data-add="${item.id}">+</button>
             </div>`
     })
 }
