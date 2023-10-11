db55c2ec83d5f3abdd2929e30e01015c6e5ea341
Picasso <urvishp13@gmail.com>
2023-10-11 11:42:25 -0400

    added styling to RATING MODAL and applied a timer of disappearance after interaction with it

--- ratingFeature.js
+++ ratingFeature.js
@@ -1,31 +1,39 @@
 import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
-// give this user a unique user ID
-const uuid = uuidv4()
-
-// put the rating feature in a modal
 // give the user of this rating session a unique user ID
+const uuid = uuidv4()
+const ratingModal = document.getElementById("rating-modal")
+
 // when the user clicks on a star, turn that star and all the stars before it yellow
-    // re-render modal
-    // make modal disappear after 1.5s
+document.addEventListener('click', handleRatingClick)
 
-document.addEventListener('click', function(e) {
-    console.log(e)
-
-    const ratingStars = e.target.parentElement.children
-
+function handleRatingClick(e) {
     // if user gives a rating
     if (e.target.dataset.rating) {
-        // highlight only the stars that equate to that rating
-        for (let i = 0; i < e.target.dataset.rating; i++) {
-            ratingStars[i].classList.add('rating')
-        }
-    }
-})
+        // get all the stars
+        const ratingStars = e.target.parentElement.children
 
-function getRating(userId) {
+        // in case user wants to change rating, clear previous rating
+        for (const star of ratingStars) {
+            star.classList.remove('rated')
+        }
+        // reset the timer on making the rating modal disappear, as a change in the rating is going to be made
+        ratingModal.classList.remove('disappear')
+        ratingModal.offsetWidth /* trigger reflow */
+
+        // highlight only the stars that equate to the rating the user gave
+        for (let i = 0; i < e.target.dataset.rating; i++) {
+            ratingStars[i].classList.add('rated')
+        }
+
+        // make modal disappear after 5s after click
+        ratingModal.classList.add('disappear')
+    }
+}
+
+function getRatingHtml(userId) {
     const ratingHtml = `
-        <div id="rating-modal-${userId}" class="modal rating-modal">
+        <div id="rating-${userId}" class="rating">
             <i class="fa-solid fa-star" data-rating="1"></i>
             <i class="fa-solid fa-star" data-rating="2"></i>
             <i class="fa-solid fa-star" data-rating="3"></i>
@@ -37,10 +45,9 @@ function getRating(userId) {
 }
 
 function renderRatingModal(userId) {
-    const ratingModal = document.getElementById("rating")
-    
     ratingModal.classList.remove("hide")
-    ratingModal.innerHTML = getRating(userId)
+    // put the rating feature in a modal
+    ratingModal.innerHTML += getRatingHtml(userId)
 }
 
 export { uuid, renderRatingModal }
